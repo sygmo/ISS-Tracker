2767fb978fd2a942b2654920d1c851c3e18f4efe
sygmo <sydney.mercier91@gmail.com>
2016-12-19 11:15:34 -0600

    Preliminary setup complete

--- apps/isstracker/views.py
+++ apps/isstracker/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib import messages
+from .models import *
 
 # Create your views here.
+def index(request):
+	return render(request, 'isstracker/index.html')
+		
